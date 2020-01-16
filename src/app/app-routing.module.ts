5797e6e9d377ac4aab58b8f07eb7509466cef152
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-01-16 11:50:39 +0100

    Errors: si l'utilisateur entre une adresse url vide ou le url entrée n'existe pas, il faut toujours rediriger vers le home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,11 @@ import { SigninComponent } from './authentication/signin/signin.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },                       // localhost:4200/home
-  { path: 'admin/dashboard', component: AdminDashboardComponent},    // localhost:4200/admin/dashboard
-  { path: 'login', component: SigninComponent}                      // localhost:4200/login
+  { path: 'admin/dashboard', component: AdminDashboardComponent},   // localhost:4200/admin/dashboard
+  { path: 'login', component: SigninComponent},                     // localhost:4200/login
+  {path: '', redirectTo: 'home', pathMatch: 'full'},                // path vide (localhost:4200) redirection sur la page home
+  {path: '**', redirectTo: 'home'}                                  // path non vide et non existant (localhost:4200/blablabla)
+                                                                    // redirection sur la page home
 ];
 
 @NgModule({
